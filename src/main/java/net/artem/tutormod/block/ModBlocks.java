47afed7022415deab26a187163547a7539350d70
ToKeIIIa <atokarev476@gmail.com>
2025-09-29 17:55:21 +0700

    Changed some things

--- src/main/java/net/artem/tutormod/block/ModBlocks.java
+++ src/main/java/net/artem/tutormod/block/ModBlocks.java
@@ -3,10 +3,12 @@ package net.artem.tutormod.block;
 import net.artem.tutormod.TutorMod;
 import net.artem.tutormod.item.ModelItems;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -15,9 +17,10 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(BuiltInRegistries.BLOCK, TutorMod.MOD_ID);
 
-    // Регистрируем сам блок
+    // Регистрируем руду бисмута (теперь с опытом при добыче)
     public static final DeferredHolder<Block, Block> BISMUTH_ORE = BLOCKS.register("bismuth_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)));
+            () -> new DropExperienceBlock(UniformInt.of(2, 5),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)));
 
     // Регистрируем предмет блока
     public static final DeferredHolder<Item, Item> BISMUTH_ORE_ITEM = ModelItems.ITEMS.register("bismuth_ore",
